6a3d5789d46aa8246eeb997f5e6f13ee5ba79a07
Louis Jarvis <ljar069@aucklanduni.ac.nz>
2023-07-23 21:10:25 +1200

    move quality_df and initial_solution into data folder as csvs

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
-from dataclasses import dataclass, field
-from typing import List, Generator, Any
+from dataclasses import dataclass
+from typing import List
 import numpy as np
 import pandas as pd
 import itertools
 import warnings
 from copy import deepcopy
 from abc import ABC, abstractmethod
+from pathlib import Path
+
+INITIAL_SOL_PATH = Path("data/initial_solution.csv")
+QUALITY_DATA_PATH = Path("data/quality_df.csv")
 
 
 # there are 51 pots
@@ -61,17 +65,15 @@ class Swap2Pots(NeighbourhoodRule):
     - c1 is the index of the first crucible
     - c2 is the index of the first
 
-    - where x is the current solution
+    where x is the current solution
 
     y(x, p1, p2, c1, c2) = {y1, y2, ..., y17} is a neighbour of the current solution.
 
     Neighbourhood rule:
 
-    yi = {
-        x_{p2} if i = p1 & (c1 != c2)
-        x_{p1} if i = p2 & (c1 != c2)
-        x_{i} otherwise
-        }
+    yi = x_p1 if i = p1 & (c1 != c2)
+       = x_p2 if i = p2 & (c1 != c2)
+       = xi otherwise
     """
 
     def __init__(self, verbose=False) -> None:
@@ -85,7 +87,6 @@ class Swap2Pots(NeighbourhoodRule):
 
         for c1, c2 in crucible_indices:
             if c1 != c2:
-                # TODO verbose option
                 for p1, p2 in pot_indices:
                     if p1 != p2:
                         temp = x_i[c1][p1]
@@ -171,118 +172,9 @@ class NextAscent:
         return delta_1 + delta_2
 
 
-# TODO store this in a csv, read in...
-# Intitial Solution
-PotAl = [
-    99.79,
-    99.23,
-    99.64,
-    99.88,
-    99.55,
-    99.87,
-    99.55,
-    99.19,
-    99.76,
-    99.70,
-    99.26,
-    99.60,
-    99.05,
-    99.49,
-    99.69,
-    99.48,
-    99.60,
-    99.89,
-    99.39,
-    99.48,
-    99.77,
-    99.57,
-    99.48,
-    99.85,
-    99.09,
-    99.64,
-    99.71,
-    99.59,
-    99.14,
-    99.87,
-    99.38,
-    99.56,
-    99.32,
-    99.55,
-    99.61,
-    99.57,
-    99.75,
-    99.63,
-    99.17,
-    99.97,
-    99.74,
-    99.49,
-    99.75,
-    99.40,
-    99.72,
-    99.95,
-    99.31,
-    99.55,
-    99.29,
-    99.09,
-    99.20,
-]
+def create_init_sol(df=pd.read_csv(INITIAL_SOL_PATH)):
+    pot_al, pot_fe = df["PotAl"], df["PotFe"]
 
-PotFe = [
-    0.01,
-    0.68,
-    0.25,
-    0.61,
-    0.13,
-    0.77,
-    0.48,
-    0.18,
-    0.66,
-    0.43,
-    0.13,
-    0.87,
-    0.96,
-    0.47,
-    0.51,
-    0.73,
-    0.04,
-    0.76,
-    0.89,
-    0.90,
-    0.96,
-    0.73,
-    0.88,
-    0.43,
-    0.60,
-    0.37,
-    0.51,
-    0.26,
-    0.30,
-    0.46,
-    0.21,
-    0.77,
-    0.43,
-    0.52,
-    0.63,
-    0.76,
-    0.02,
-    0.75,
-    0.90,
-    0.53,
-    0.14,
-    0.10,
-    0.31,
-    0.20,
-    0.45,
-    0.67,
-    0.56,
-    0.24,
-    0.72,
-    0.56,
-    0.01,
-]
-
-
-def create_init_sol(pot_al=PotAl, pot_fe=PotFe):
     sol = []
     fx = 0
 
@@ -301,52 +193,9 @@ def create_init_sol(pot_al=PotAl, pot_fe=PotFe):
     return sol, fx
 
 
-quality_df = pd.DataFrame(
-    data={
-        "QualityMinAl": [
-            95.00,
-            99.10,
-            99.10,
-            99.20,
-            99.25,
-            99.35,
-            99.50,
-            99.65,
-            99.75,
-            99.85,
-            99.90,
-        ],
-        "QualityMaxFe": [
-            5.00,
-            0.81,
-            0.79,
-            0.79,
-            0.76,
-            0.72,
-            0.53,
-            0.50,
-            0.46,
-            0.33,
-            0.30,
-        ],
-        "QualityValue": [
-            10,
-            21.25,
-            26.95,
-            36.25,
-            41.53,
-            44.53,
-            48.71,
-            52.44,
-            57.35,
-            68.21,
-            72.56,
-        ],
-    }
-)
-
-
-def calc_crucible_value(crucible: Crucible, quality_df=quality_df, tol=1e-5):
+def calc_crucible_value(
+    crucible: Crucible, quality_df=pd.read_csv(QUALITY_DATA_PATH), tol=1e-5
+):
     value = 0
     # TODO vectorise this
     for i in range(len(quality_df) - 1, 0, -1):
